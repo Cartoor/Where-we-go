4b0a04ee152c4d28d78052fccaf766512c18b81e
Cartoor <ttwodoges@gmail.com>
2020-04-20 22:21:50 +0800

    City css finish

---
+++ src/Pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="alphabet-list">
+    <li class="alphabet-item">A</li>
+    <li class="alphabet-item">A</li>
+    <li class="alphabet-item">A</li>
+    <li class="alphabet-item">A</li>
+    <li class="alphabet-item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus">
+  @import '~styles/variables.styl'
+  .alphabet-list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .alphabet-item
+      line-height .44rem
+      text-align center
+      color $bgColor
+</style>
