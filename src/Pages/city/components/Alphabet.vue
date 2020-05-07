8235105ad70cf1bd506a500e73f0e5e18b82f69e
Cartoor <ttwodoges@gmail.com>
2020-05-07 11:22:20 +0800

    bro-communicate over

--- src/Pages/city/components/Alphabet.vue
+++ src/Pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
   <ul class="alphabet-list">
-    <li class="alphabet-item"  v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li
+      class="alphabet-item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      >{{item}}</li>
   </ul>
 </template>
 
@@ -9,6 +18,41 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index <= this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
@@ -25,7 +69,7 @@ export default {
     bottom 0
     width .4rem
     .alphabet-item
-      line-height .44rem
+      line-height .4rem
       text-align center
       color $bgColor
 </style>
