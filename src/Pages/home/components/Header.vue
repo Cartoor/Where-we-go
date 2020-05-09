dd9e149b0319786260a2d1706b5435484eb19dc3
Cartoor <ttwodoges@gmail.com>
2020-05-09 13:57:00 +0800

    vuex done

--- src/Pages/home/components/Header.vue
+++ src/Pages/home/components/Header.vue
@@ -17,10 +17,12 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city'])
   }
 }
 </script>
@@ -49,7 +51,8 @@ export default {
     color: #ccc
     flex: 1
   .header-right
-    width: 1.26rem
+    min-width: 1.04rem
+    padding 0 0.1rem
     float: right
     text-align: center
     color: #fff
